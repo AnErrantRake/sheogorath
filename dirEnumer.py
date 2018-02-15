3ba24dcf53cd73a089cdd32a77949becec887839
Stephen Peterson <stephenanderspeterson@gmail.com>
2018-02-15 09:11:11 -0600

    Utility changes

--- dirEnumer.py
+++ dirEnumer.py
@@ -1,19 +1,21 @@
 import os, time
 
 # string output, pipe to store
-# outputs '|' delimited strings
-# enumerates a file structure for 'dir'
+
+# intended for tracking contents of large/complicated file structures
+# enumerates a file structure for 'dir' in csv format
 # created time might not provide expected result on Unix
 
 dir = "path/to/dir"
+delim = "|"
 
 def list_files(inpath):
-    print("FileName|CurrentPath|Created|LastTouched|Category|Summary")
+    print("FileName%sCurrentPath%sCreated%sLastTouched%sCategory%sSummary" % (delim,delim,delim,delim,delim)
     for root, drop, filenames in os.walk(inpath):
         abspath = os.path.abspath(root)
         for filename in filenames:
             created = time.asctime(time.localtime(os.path.getctime(os.path.join(root, filename))))
             modified = time.asctime(time.localtime(os.path.getmtime(os.path.join(root, filename))))
-            print("%s|%s|%s|%s|%s|%s" % (filename, abspath,created,modified,abspath," "))
+            print(filename + delim + abspath + delim + created + delim + modified + delim abspath + delim)
 
 list_files(dir)
